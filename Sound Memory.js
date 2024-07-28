4b7348ca2987cc1040b036f8f647eb798891c4b3
Csw <66208458+meronemo@users.noreply.github.com>
2024-07-29 03:01:07 +0900

    completed game

--- Sound Memory.js
+++ Sound Memory.js
@@ -1,6 +1,8 @@
 /*
-First time? Check out the tutorial game:
-https://sprig.hackclub.com/gallery/getting_started
+Sound Memory
+Listen and memorize the order of the sound playing.
+After the tunes are all played, press the key in the same order.
+Red-W, Yellow-A, Green-S, Blue-D
 
 @title: Sound Memory
 @author: meronemo
@@ -160,19 +162,14 @@ const level = map`
 .a.
 b.d
 .c.`;
-
 setMap(level);
 
-onInput("s", () => {
-  getFirst(player).y += 1
-});
-
-afterInput(() => {
-
-});
-
-const sound = [];
+let sound = [];
 let round = 0;
+let getKey = 0;
+let pressed = 0;
+let count = 0;
+let gameover = 0;
 
 const sounda = tune`
 500: B5-500,
@@ -186,17 +183,67 @@ const soundc = tune`
 const soundd = tune`
 500: C4-500,
 15500`;
+const over = tune`
+76.92307692307692: B5-76.92307692307692,
+76.92307692307692: G5-76.92307692307692,
+76.92307692307692: F5-76.92307692307692,
+76.92307692307692: D5-76.92307692307692,
+76.92307692307692: C5-76.92307692307692,
+76.92307692307692: A4-76.92307692307692,
+76.92307692307692: G4-76.92307692307692,
+76.92307692307692: E4-76.92307692307692,
+76.92307692307692: D4-76.92307692307692,
+76.92307692307692: C4-76.92307692307692,
+76.92307692307692: C4-76.92307692307692,
+76.92307692307692: C4-76.92307692307692,
+76.92307692307692: C4-76.92307692307692,
+1461.5384615384614`;
 
-function nextRound() {
+onInput("w", () => {
+  pressed = 1;
+});
+
+onInput("a", () => {
+  pressed = 2;
+});
+
+onInput("s", () => {
+  pressed = 3;
+});
+
+onInput("d", () => {
+  pressed = 4;
+});
+
+async function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true)
+    }, ms);
+  });
+}
+
+function clear() {
+  if (getFirst(sela)) getFirst(sela).type = boxa;
+  if (getFirst(selb)) getFirst(selb).type = boxb;
+  if (getFirst(selc)) getFirst(selc).type = boxc;
+  if (getFirst(seld)) getFirst(seld).type = boxd;
+}
+
+async function nextRound() {
   round += 1;
+  getKey = 0;
+  
   const random = Math.floor(Math.random() * 4) + 1;
   sound.push(random);
   console.log(sound);
   
   clearText();
   addText("R" + String(round), { y: 7, color: color`0` });
-
+  addText("Listen", { y: 8, color: color`0` });
+  
   for (let i=0; i<sound.length; i++) {
+    await sleep(300);
     if (sound[i] === 1) {
       playTune(sounda);
       getFirst(boxa).type = sela;
@@ -210,6 +257,84 @@ function nextRound() {
       playTune(soundd);
       getFirst(boxd).type = seld;
     }
+    await sleep(300);
+    clear();
+  }
+  
+  await sleep(100);
+
+  clearText();
+  addText("R" + String(round), { y: 7, color: color`0` });
+  addText("Repeat", { y: 8, color: color`0` });
+  clear();
+  getKey = 1;
+}
+
+async function gameOver() {
+  getKey = 0;
+  playTune(over);
+  for (let i=0; i<=2; i++) {
+    for (let j=0; j<=2; j++) {
+      clearTile(i, j);
+    }
+  }
+  clearText();
+  addText("GAME OVER", { y: 6, color: color`3` });
+  addText("Round: " + String(round), { y: 7, color: color`0` });
+  addText("Press any key", { y: 9, color: color`0` });
+  addText("to play again", { y: 10, color: color`0` });
+  await sleep(500);
+  gameover = 1;
+}
+
+async function checkKey() {
+  if (gameover) {
+    round = 0;
+    count = 0;
+    gameover = 0;
+    sound = [];
+    setMap(level);
+    nextRound();
+  }
+  if (!getKey) return;
+
+  if (pressed === 1) {
+    playTune(sounda);
+    getFirst(boxa).type = sela;
+    await sleep(100);
+    getFirst(sela).type = boxa;
+  } else if (pressed === 2) {
+    playTune(soundb);
+    getFirst(boxb).type = selb;
+    await sleep(100);
+    getFirst(selb).type = boxb;
+  } else if (pressed === 3) {
+    playTune(soundc);
+    getFirst(boxc).type = selc;
+    await sleep(100);
+    getFirst(selc).type = boxc;
+  } else if (pressed === 4) {
+    playTune(soundd);
+    getFirst(boxd).type = seld;
+    await sleep(100);
+    getFirst(seld).type = boxd;
+  }
+  
+  if (sound[count] !== pressed) {
+    gameOver();
+  } else if (count === sound.length - 1) {
+    getKey = 0;
+    clearText();
+    addText("R" + String(round), { y: 7, color: color`0` });
+    addText("Great!", { y: 8, color: color`4` });
+    await sleep(1000);
+    count = 0;
+    nextRound();
+  } else {
+    count += 1;
   }
 }
+
+afterInput(() => checkKey());
+
 nextRound();
